8b09c9f599dc13bd322b9fbb3e3b35871b18c7c7
ikermani15 <iker.maniega@email.com>
2026-08-10 11:18:10 +0200

    Add Vercel Analytics

--- Question/src/main.jsx
+++ Question/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Analytics } from "@vercel/analytics/react"
 import { LangProvider } from "./context/LangContext"
 import { GroupProvider } from "./context/GroupContext"
 import App from "./App.jsx"
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
     <LangProvider>
       <GroupProvider>
         <App />
+        <Analytics />
       </GroupProvider>
     </LangProvider>
   </StrictMode>
